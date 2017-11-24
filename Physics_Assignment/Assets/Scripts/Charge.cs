551ab67db01f0ecccc0e256f1ff970927b903261
Jack Wiebe <jack.b.wiebe@gmail.com>
2017-11-23 22:22:41 -0500

    initial

---
+++ Physics_Assignment/Assets/Scripts/Charge.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Charge : MonoBehaviour {
+
+	public GameObject frame;
+	public float m_ChargeAmount;
+	public IEnumerator charge_co;
+	// Use this for initialization
+	void Start () {
+		Toggle_Visibility ();
+		m_ChargeAmount = 0;
+		charge_co = Charge_Up();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale = new Vector3 (m_ChargeAmount/50,this.transform.localScale.y, this.transform.localScale.z);
+	}
+
+	public void Toggle_Visibility()
+	{
+		transform.parent.gameObject.GetComponent<Image> ().enabled = !transform.parent.gameObject.GetComponent<Image> ().enabled;
+		this.GetComponent<Image> ().enabled = !this.GetComponent<Image> ().enabled;
+	}
+
+	IEnumerator Charge_Up()
+	{
+		while(m_ChargeAmount < 50)
+		{
+			m_ChargeAmount += 1;
+			yield return new WaitForSeconds (.1f);
+		}
+	}
+}
---
+++ Physics_Assignment/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Charge charge;
+	public GameObject Stone;
+	public float mass;
+
+	void Start () {
+		
+	}
+
+	public void Change_Mass(float value)
+	{
+		Stone.GetComponent<Rigidbody> ().mass=value * 10;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Handle_Input ();
+	}
+
+	void Handle_Input()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			charge.Toggle_Visibility ();
+			StartCoroutine (charge.charge_co);
+		}
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			StopCoroutine (charge.charge_co);
+			charge.Toggle_Visibility ();
+			Set_Stats ();
+		}
+	}
+
+	private float Calculate_Force ()
+	{
+		return Stone.GetComponent<Rigidbody> ().mass*charge.m_ChargeAmount;
+	}
+
+	private void Set_Stats ()
+	{
+		Stone.GetComponent<Rigidbody> ().AddForce (Vector3.forward * Calculate_Force(), ForceMode.Impulse);
+	}
+
+
+}
